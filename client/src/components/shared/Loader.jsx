9885ec8f0bfd043e3b0d3a4812d88884d53dd226
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-18 18:18:13 +0600

    feat: implement loader file

---
+++ client/src/components/shared/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center min-h-[300px]">
+      <h1 className="text-3xl font-bold">Loading...</h1>
+    </div>
+  );
+};
+
+export default Loader;
