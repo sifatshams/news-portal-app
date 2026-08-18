b158653e8aed4cc6f91ca9c7289b0aad9e1e265f
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-18 17:56:16 +0600

    feat: implement transporter ports and codes

---
+++ server/src/config/transporter.js
@@ -0,0 +1,19 @@
+import nodemailer from 'nodemailer';
+
+export const transporter = nodemailer.createTransport({
+  host: process.env.BREVO_HOST,
+  port: parseInt(process.env.BREVO_PORT),
+  secure: false,
+  auth: {
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS,
+  },
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('SMTP connected successfully');
+  }
+});
