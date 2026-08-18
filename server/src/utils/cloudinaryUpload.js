02ee7bfa1d1f5baf72b0aa382adfc453cc467b0c
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-18 17:54:54 +0600

    feat: implement cloudinary logic for upload images

---
+++ server/src/utils/cloudinaryUpload.js
@@ -0,0 +1,15 @@
+import cloudinary from '../config/cloudinaryConfig.js';
+
+export const uploadToCloudinary = (buffer, folder) => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      { folder },
+      (error, result) => {
+        if (error) return reject(error);
+        resolve(result);
+      },
+    );
+
+    stream.end(buffer);
+  });
+};
